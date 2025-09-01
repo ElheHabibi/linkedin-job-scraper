09fb0c8133c4d5ef58c96e1eb9f006edff31a4fa
ElheHabibi <elheh.habibi@gmail.com>
2025-09-01 17:40:42 +0330

    Display total jobs found

--- scraper.py
+++ scraper.py
@@ -33,6 +33,7 @@ class Job_Listing_Scraper:
 
     def extract_data(self):
         cards = self.driver.find_elements(By.CLASS_NAME, 'base-card')[:self.results_num]
+        print(f"\n💎 {len(cards)} jobs are found for '{self.job_title}'.\n")
 
         for card in cards:
             try:
@@ -61,13 +62,14 @@ class Job_Listing_Scraper:
                 "Location": location,
                 "Link": link
             })
+            
         return True 
 
     def save_data(self, filename=None):
         if not self.results: 
             return 
 
-        safe_title = "_".join(self.job_title.strip().lower().split())
+        safe_title = "_".join(self.job_title.strip().split())
         filename = f"results/jobs_{safe_title}.csv"
 
         dataframe = pd.DataFrame(self.results)
@@ -124,100 +126,4 @@ while True:
         break       
         
         
-        
-        
 
-# job_title = input("🥸  Enter the job title: ")
-
-# while True:
-#     input_num = input("🥸  How many results do you want? ")
-#     try:
-#         results_num = int(input_num)
-#         if results_num <= 0:
-#             print("☹️  Please enter a valid positive number.")
-#             continue
-#         break  
-#     except ValueError:
-#         print("☹️  Please enter a valid number.")
-
-# scraper = Job_Listing_Scraper(job_title, results_num)
-# scraper.search_jobs()
-# scraper.extract_data()
-# scraper.save_data()
-# scraper.quit()
-        
-        
-###################################################
-# linear job scraper code:
-                
-
-# job_title = input("🥸  Enter the job title: ")
-# input_num = input("🥸  How many results do you want? ")
-
-# try:
-#     num_results = int(input_num)
-#     if num_results <= 0:
-#         raise ValueError
-# except ValueError:
-#     raise SystemExit("☹️  Please enter a valid positive number.")
-
-# driver = webdriver.Chrome()
-
-# query = job_title.replace(" ", "%20")
-# driver.get(f"https://www.linkedin.com/jobs/search/?keywords={query}")
-
-# WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'base-card')))
-
-# for i in range(5):
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     time.sleep(2)
-
-# cards = driver.find_elements(By.CLASS_NAME, 'base-card')[:num_results]
-
-# print(f"\n💎 {len(cards)} jobs are found for '{job_title}'.\n")
-
-
-# i = 1
-# results =[]
-# for card in cards:
-#     try:
-#         job = card.find_element(By.CSS_SELECTOR, 'h3.base-search-card__title').get_attribute("innerText").strip()
-#     except:
-#         job = "not found"
-
-#     try:
-#         company = card.find_element(By.CSS_SELECTOR, 'h4.base-search-card__subtitle a').get_attribute("innerText").strip()
-#     except:
-#         company = "not found"
-
-#     try:
-#         location = card.find_element(By.CSS_SELECTOR, 'span.job-search-card__location').get_attribute("innerText").strip()
-#         if "," in location:
-#             location = location.split(",")[0].strip()
-#     except:
-#         location = "not found"
-
-#     try:
-#         link = card.find_element(By.CSS_SELECTOR, 'a.base-card__full-link').get_attribute("href")
-#     except:
-#         link = "not found"
-
-#     results.append({
-#         "Job Title": job,
-#         "Company": company,
-#         "Location": location,
-#         "Link": link
-#     })
-#     i += 1
-    
-# driver.quit()
-
-# if results:
-#     safe_title = "_".join(job_title.strip().lower().split())
-#     filename = f"jobs_{safe_title}.csv"
-
-#     jobs = pd.DataFrame(results)
-#     jobs.to_csv(filename, index=False)
-#     print(f"\n😍  Jobs are saved in {filename}")
-# else:
-#     print("☹️  No jobs found.")
